08c3ddde5dd42c82e3c8c10741c6845784f4a050
mr-yoso <112569191+mr-yoso@users.noreply.github.com>
2024-11-07 00:27:23 -0500

    added weapons and a gun script

---
+++ ROOMS/Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCamera;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            // Enemy enemy = hit.transform.GetComponent<Enemy>();
+            // if (enemy != null)
+            // {
+            //     enemy.TakeDamage(damage);
+            // }
+        }
+    }
+}
--- ROOMS/Assets/Scripts/MouseMovement.cs
+++ ROOMS/Assets/Scripts/MouseMovement.cs
@@ -6,36 +6,32 @@ public class MouseMovement : MonoBehaviour
 {
     public float mouseSensitivity = 500f;
 
-    public Transform playerBody;
-
     float xRotation = 0f;
 
     public float topClamp = -90f;
-    
+
     public float bottomClamp = 90f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Locking the cursor to the middle of the screen and making it invisible
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Getting the mouse inputs
+        Look();
+    }
+
+    void Look()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Rotation around the x axis (Look up and down)
         xRotation -= mouseY;
 
-        // Clamp the rotation
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
 
-        // Apply rotations to our transform
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.parent.Rotate(Vector3.up * mouseX);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
