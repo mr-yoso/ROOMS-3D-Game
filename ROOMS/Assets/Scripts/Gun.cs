5b43c5a715ca2297f97700b005e6c458bf90534f
Rolly Jake Gayo <rj0gayo@gmail.com>
2024-12-02 15:17:38 -0500

    added a package of particle system

--- ROOMS/Assets/Scripts/Gun.cs
+++ ROOMS/Assets/Scripts/Gun.cs
@@ -4,8 +4,9 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    
     public Camera fpsCamera;
-
+    public ParticleSystem muzzleFlash;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -16,6 +17,8 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
+        muzzleFlash.Play();
+        
         RaycastHit hit;
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
