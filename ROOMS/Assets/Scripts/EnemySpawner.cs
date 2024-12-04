f030641346eca3ea1e2a197bca78789941dbb3c7
Jodel Anthony Briones <jodelbriones401@gmail.com>
2024-12-04 18:18:24 -0500

    added mob spawner in level 1

---
+++ ROOMS/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float interval = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy(interval, enemy));
+    }
+
+    // Update is called once per frame
+    private IEnumerator SpawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-8f, 8f), 0, Random.Range(-8f, 8f)), Quaternion.identity);
+        StartCoroutine(SpawnEnemy(interval, enemy));
+    }
+}
